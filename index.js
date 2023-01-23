80f8972acbf5123da681df2b77ef187a865f9e52
Mrunal Awate <122474964+MrunalAwate@users.noreply.github.com>
2023-01-23 15:30:19 +0530

    Update index.js
    code updated for mongoose.connect

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ app.listen(process.env.PORT || 5000, () => {
     console.log('backend running')
 })
 
-mongoose.connect(CYCLIC_URL)
+/*mongoose.connect(CYCLIC_URL)
 .then(() => console.log(`Server running on port: ${PORT}`))
-.catch((error) => console.log('error connecting', error))
+.catch((error) => console.log('error connecting', error))*/
 
 //const PORT = process.env.PORT || 5000;
 
 //mongoose.set('strictQuery', true);
 
-/*mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));*/
+    .catch((error) => console.log(error.message))
 
