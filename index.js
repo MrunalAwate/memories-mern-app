0f439a65ec963bab5833ef678013dfa483fbbf2c
Mrunal Awate <122474964+MrunalAwate@users.noreply.github.com>
2023-01-23 16:03:36 +0530

    Updated for strictQurery

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.listen(process.env.PORT || 5000, () => {
 
 //const PORT = process.env.PORT || 5000;
 
-//mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
