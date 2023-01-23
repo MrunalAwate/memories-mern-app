92920dd33674893dffe1b71603056200e7985e2a
Mrunal Awate <122474964+MrunalAwate@users.noreply.github.com>
2023-01-23 19:05:56 +0530

    updated port

--- index.js
+++ index.js
@@ -22,11 +22,9 @@ app.use('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+/*app.listen(PORT, () => {
     console.log('backend running')
-})
-
-
+})*/
 
 mongoose.set('strictQuery', true);
 
